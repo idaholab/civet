bbb02f4d94087797f2cfff279e0b7e304eb3a9cb
Brian Alger <brian.alger@inl.gov>
2016-07-27 09:55:31 -0600

    Add license/copyright information files and a header to all files. Most of the headers where generated by:
    copyright-header --license-file copyright_header.txt --add-path ci:client:civet:manage.py --output-dir ./
    
    This wasn't good enough for the *.html files. They were changed to use
    the {% comment %} template tag. Also, the python files needed a newline
    at EOF.

--- ci/gitlab/views.py
+++ ci/gitlab/views.py
@@ -1,3 +1,18 @@
+
+# Copyright 2016 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 import logging, traceback
@@ -208,4 +223,3 @@ def webhook(request, build_key):
     err_str = "Invalid call to gitlab/webhook for build key %s. Error: %s" % (build_key, traceback.format_exc(e))
     logger.warning(err_str)
     return HttpResponseBadRequest(err_str)
-
