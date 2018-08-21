13260a98159ca169752ead3fe29e0283309326ab
Brian Alger <brian.alger@inl.gov>
2018-08-21 14:30:49 -0600

    Update live tests with correct claim response

--- ci/management/commands/generate_test_job_files.py
+++ ci/management/commands/generate_test_job_files.py
@@ -18,75 +18,52 @@ from django.core.management.base import BaseCommand
 from ci.tests import utils
 from ci.client import views
 from django.test.client import RequestFactory
+from django.test import override_settings
 from django.urls import reverse
 from django.conf import settings
 import shutil, os
 import json
+import traceback
 
 class Command(BaseCommand):
-    help = "Generate the json files for use with client testing. The resultant *.json files should be moved to client/tests/"
+    help = "Generate the claim_response.json file that the client testing uses."
 
+    @override_settings(INSTALLED_GITSERVERS=[utils.github_config()])
     def handle(self, *args, **options):
         factory = RequestFactory()
-        user = utils.get_test_user()
         client = utils.create_client()
-        test_job = utils.create_job(user=user)
-        test_job.ready = True
-        test_job.client = None
-        test_job.status = 0
-        test_job.save()
+        job = utils.create_job()
+        user = job.recipe.build_user
 
         orig_recipe_dir = settings.RECIPE_BASE_DIR
-        recipe_dir, repo = utils.create_recipe_dir()
+        recipe_dir = utils.create_recipe_dir()
         settings.RECIPE_BASE_DIR = recipe_dir
 
-        # create a prestep to make sure sourcing functions work
-        prestep0 = utils.create_prestepsource(filename="prestep0.sh", recipe=test_job.recipe)
-        with open(os.path.join(recipe_dir, prestep0.filename), "w") as f:
-            f.write('function start_message()\n{\n  echo start "$*"\n}')
-
-        # create a prestep to make sure sourcing functions work
-        prestep1 = utils.create_prestepsource(filename="prestep1.sh", recipe=test_job.recipe)
-        with open(os.path.join(recipe_dir, prestep1.filename), "w") as f:
-            f.write('function end_message()\n{\n  echo "$*"\n}')
-
-        # create a global environment variable to test env works
-        # as well as BUILD_ROOT replacement
-        utils.create_recipe_environment(name="GLOBAL_NAME", value="BUILD_ROOT/global", recipe=test_job.recipe)
-        count = 0
-        for s in ["step0", "step1", "step2"]:
-            step = utils.create_step(name=s, recipe=test_job.recipe, position=count)
-            # create a step environment variable to test env works
-            # as well as BUILD_ROOT replacement
-            utils.create_step_environment(name="STEP_NAME", value="BUILD_ROOT/%s" % s, step=step)
-            step.filename = "%s.sh" % s
-            step.save()
-            count += 1
-            script_filename = os.path.join(recipe_dir, step.filename)
-            with open(script_filename, "w") as f:
-                self.stdout.write("Writing to %s" % script_filename)
-                f.write("echo $GLOBAL_NAME $STEP_NAME\nstart_message {0}\nsleep 1\nend_message {0}\n".format(s))
-
-        # Everything is written to the fake recipe dir, get the request response
         try:
-            request = factory.get(reverse("ci:client:ready_jobs", args=[user.build_key, client.name]))
+            args=[user.build_key, client.name]
+            request = factory.get(reverse("ci:client:ready_jobs", args=args))
             reply = views.ready_jobs(request, user.build_key, client.name)
-            if reply.status_code == 200:
-                with open("ready_jobs.json", "wt") as f:
-                    json.dump(json.loads(reply.content), f, indent=2, sort_keys=True)
 
-            claim_job_url = reverse("ci:client:claim_job", args=[user.build_key, test_job.config.name, client.name])
-            request = factory.post(claim_job_url, json.dumps({"job_id": test_job.pk}), content_type="application/json")
-            reply = views.claim_job(request, user.build_key, test_job.config.name, client.name)
+            args = [user.build_key, job.config.name, client.name]
+            claim_job_url = reverse("ci:client:claim_job", args=args)
+            data = json.dumps({"job_id": job.pk})
+            request = factory.post(claim_job_url, data, content_type="application/json")
+            reply = views.claim_job(request, user.build_key, job.config.name, client.name)
 
             if reply.status_code == 200:
-                with open("claimed_job.json", "w") as f:
+                this_file = os.path.realpath(__file__)
+                test_dir = os.path.join(os.path.dirname(this_file), "..", "..", "..", "client", "tests")
+                fname = os.path.join(test_dir, "claim_response.json")
+                with open(fname, "w") as f:
                     json.dump(json.loads(reply.content), f, indent=2, sort_keys=True)
+                    f.write("\n")
             else:
-                self.stdout.write(reply.status_code)
-                self.stdou.write(reply.content)
+                self.stderr.write(reply.status_code)
+                self.stderr.write(reply.content)
         except Exception as e:
-            self.stdout.write("Error occurred: %s" % e)
-        test_job.recipe.delete()
+            self.stderr.write(traceback.format_exc())
+            self.stderr.write("Error occurred: %s" % e)
+        job.recipe.delete()
         shutil.rmtree(recipe_dir)
+        settings.RECIPE_BASE_DIR = orig_recipe_dir
         settings.RECIPE_BASE_DIR = orig_recipe_dir
