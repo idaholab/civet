a7da260698d93ffcc960da61725af18c1c65d5b6
Robert Carlsen <rwcarlsen@gmail.com>
2021-06-08 11:27:24 -0600

    use uwsgi worker ids instead of silly locking

--- civet/apps.py
+++ civet/apps.py
@@ -3,11 +3,12 @@ from django.apps import AppConfig
 import time, threading
 from croniter import croniter
 from datetime import datetime, timedelta
-from cron_descriptor import get_description, ExpressionDescriptor
 import os
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.core.management import call_command
+import logging
+import uwsgi
 
 logger = logging.getLogger('ci')
 
@@ -20,6 +21,7 @@ class scheduleConfig(AppConfig):
 
         from ci import models, ManualEvent #has to be done here, because these parts of the django app aren't initialized until ready() runs
 
+        logger.info("scheduler is starting")
         while(True):
             logger.info("SCHEDULER: checking for scheduled recipes")
             #get all recipes with schedules, to check if recipes have been changed/added since the last load
@@ -35,16 +37,17 @@ class scheduleConfig(AppConfig):
                 next_run_time = c.get_next(datetime)
 
                 if next_run_time <= now:
-                    last_run_times[r.id] = now
-                    user = get_object_or_404(models.GitUser, name=r.build_user)
+                    user = r.build_user
                     branch = r.branch
                     latest = user.api().last_sha(branch.repository.user.name, branch.repository.name, branch.name)
                     if latest: #likely need to add exception checks for this!
+                        last_run_times[r.id] = now
+                        logger.info("SCHEDULER:         job " + r.name + ", soonest run time is {}".format(next_run_time))
                         logger.info("SCHEDULER:         Firing job " + r.name + ", time is " +datetime.now().strftime("%c"))
                         mev = ManualEvent.ManualEvent(user, branch, latest, "", recipe=r)
                         mev.force = True #forces the event through even if it exists. this is because it won't rerun the same job.
                         mev.save(update_branch_status=True) #magically add the job through a blackbox
-                        logger.info("SCHEDULER:         Job: " + r.name + " scheduled next for " + cron['nextJob'].strftime("%c"))
+                        logger.info("SCHEDULER:         Job: " + r.name + " scheduled next for {}".format(c.get_next(datetime)))
 
             # Sleep for n minutes
             #dt = (interval.get_next(datetime)-datetime.now()).total_seconds()
@@ -53,5 +56,6 @@ class scheduleConfig(AppConfig):
 
     def ready(self):
         if os.environ.get('RUN_MAIN', None) != 'true': #prevents the scheduler from running twice, django runs TWO instances of apps by default
-            logger.info("schedule loaded")
-            threading.Thread(target=scheduleConfig.schedulePinger, args=()).start()
+            if uwsgi.worker_id() == 1:
+                threading.Thread(target=scheduleConfig.schedulePinger, args=(), daemon=True).start()
+
