812076726164d8e7c41b3e2ffbe4c2774873471c
Logan Harbour <loganhharbour@gmail.com>
2024-08-30 22:43:37 -0600

    Add prefix, remove colon

--- client/INLClient.py
+++ client/INLClient.py
@@ -185,12 +185,12 @@ class INLClient(BaseClient.BaseClient):
                 # Remove the newline
                 if output_line and output_line[-1] == '\n':
                     output_line = output_line[:-1]
-                logger.info(output_line)
+                logger.info(f'CLEANUP: {output_line}')
             returncode = 0
         except subprocess.CalledProcessError as e:
             returncode = e.returncode
 
-        logger.info(f'Cleanup command completed with exit code {returncode}:')
+        logger.info(f'Cleanup command completed with exit code {returncode}')
         if returncode != 0:
             raise BaseClient.ClientException('Cleanup command failed')
 
