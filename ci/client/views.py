0f6fbc5456efd76c3a272c07e3465195b6284f3e
Logan Harbour <loganhharbour@gmail.com>
2024-09-03 11:17:33 -0600

    Use correct forwarded address
    refs #587

--- ci/client/views.py
+++ ci/client/views.py
@@ -34,7 +34,7 @@ logger = logging.getLogger('ci')
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
+        ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
